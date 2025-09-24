fd0863b02a857ce65fad2f29f4f7d66dcfda7f53
Eros-afk <erosryansimette@alu.ufc.br>
2025-09-24 20:32:27 -0300

    #

---
+++ Primeria_Forma_Normal.sql
@@ -0,0 +1,16 @@
+/*Primeira Forma Normal*/
+
+1 - Todo campo vetorizado se torna outra tabela.
+[Amarelo, Azul, Verde] -> Tabela Cor
+[Carro, Moto, Caminhão] -> Tabela Veículo
+
+2 - Todo campo multivalorado se torna outra tabela.
+Quando o campo for divisivel
+
+3 - Toda tabela necessita de pelo menos um campo que identifique todo registro como sendo unico. Isso é uma primary key. 
+
+4 - Obrigatoriedade / Cardinalidade
+(0,n) -> Opcional e pode ter muitos
+(1,n) -> Obrigatorio e pode ter muitos
+(0,1) -> Opcional e pode ter no maximo um
+(1,1) -> Obrigatorio e so pode ter um
